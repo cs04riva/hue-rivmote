8e5e1164e4d7a371a097530aec56f3e989a7e8a6
Chris Rivadeneira <chrisrivadeneira@outlook.com>
2019-03-12 22:40:47 -0400

    Simple on/off buttons for patio lights.
    Next steps - properly await http response.

--- hue-rivmote/MainPage.xaml.cs
+++ hue-rivmote/MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,52 @@ namespace hue_rivmote
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void LightOffClick(object sender, RoutedEventArgs e)
+        {
+            /*ContentDialog startingHueCommands = new ContentDialog
+            {
+                Title = "Hue App Message",
+                Content = "Starting Hue Commands.",
+                CloseButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await startingHueCommands.ShowAsync();*/
+            HttpClient httpClient = new HttpClient();
+
+            Uri requestUri = new Uri("http://192.168.1.164/api/RXsOaJ-zfHPTSZiZcdXjzsImuWrP1EIKgmtnzn97/lights/3/state");
+
+            HttpStringContent lightCommand = new HttpStringContent(
+                "{\"on\":false}",
+                Windows.Storage.Streams.UnicodeEncoding.Utf8,
+                "application/json");
+
+            HttpResponseMessage response = await httpClient.PutAsync(requestUri, lightCommand);
+
+            /*ContentDialog responseMessage = new ContentDialog
+            {
+                Title = "Hue App Message",
+                Content = respone.StatusCode.ToString(),
+                CloseButtonText = "Ok"
+            };           
+
+            ContentDialogResult result = await responseMessage.ShowAsync();*/
+        }
+
+        private async void LightOnClick(object sender, RoutedEventArgs e)
+        {
+
+            HttpClient httpClient = new HttpClient();
+
+            Uri requestUri = new Uri("http://192.168.1.164/api/RXsOaJ-zfHPTSZiZcdXjzsImuWrP1EIKgmtnzn97/lights/3/state");
+
+            HttpStringContent lightCommand = new HttpStringContent(
+                "{\"on\":true}",
+                Windows.Storage.Streams.UnicodeEncoding.Utf8,
+                "application/json");
+
+            HttpResponseMessage response = await httpClient.PutAsync(requestUri, lightCommand);
         }
     }
 }
